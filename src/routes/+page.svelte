a8392c0bb6418a14d6b2503fffcbc1eec51f29b3
Kristina <kiegorova04@mail.ru>
2024-10-05 10:23:29 +0300

    2st lesson

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script>
+    import './../reset.css'
+</script>
+
 <header>Шапка сайта</header>
 <main>
-    <h1>Большой и единсвтенный загаловок h1 на старнице</h1>
+    <h1>Большой и единсвтенный загаловок h1</h1>
 
     <h2>Списки</h2>
     <ul>
@@ -14,12 +18,14 @@
         <li>Пункт нумерованного списка</li>
     </ol>
 
+    <q class="for_list">Но во втором списке мы убрали цифры через css с помощью свойства <span class="code">list-style: none;</span> для элементов li</q>
+
     <h2>Текст и его форматирование</h2>
     <p>Lorem ipsum <strong>dolor</strong> sit amet consectetur, <em>adipisicing</em> elit. <del>Cumque</del> a et nobis similique eaque, aliquid ab obcaecati nihil placeat <u>architecto</u> in culpa <q>necessitatibus</q> doloremque quaerat explicabo velit harum voluptatum praesentium!</p>
 
     <h2>Картинка</h2>
     <figure>
-        <img src="https://www.meme-arsenal.com/memes/9b757b8a21972270989167310a59f8d7.jpg" alt="">
+        <img class="meme" src="https://www.meme-arsenal.com/memes/9b757b8a21972270989167310a59f8d7.jpg" alt="">
         <figcaption>Мем про фронтенд</figcaption>
         <button type="button">Тык</button>
     </figure>
@@ -75,7 +81,7 @@
             <option value="">Item 3</option>
         </select>
 
-        <div>
+        <div class="comment">
             <label for="">Поле для комментариев:</label>
             <textarea placeholder="Please, write here sth" name="" id="" cols="30" rows="4"></textarea>
         </div>
@@ -87,3 +93,139 @@
     <a href="https://www.google.com/">Ссылка на гугл</a>
     <a href="https://ya.ru/">Ссылка на яндекс</a>
 </footer>
+
+<style>
+    * {
+        font-family: 'Roboto', sans-serif;
+        font-size: 16px;
+        font-weight: 400;
+    }
+
+    header, main, footer {
+        padding-right: 60px;
+        padding-left: 60px;
+    }
+
+    header {
+        padding-top: 20px;
+        padding-bottom: 20px;
+        font-size: 24px;
+        background-color: #e7f2d7;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 46px;
+        margin: 20px 0;
+    }
+
+    h2 {
+        font-size: 32px;
+        margin: 40px 0 10px;
+    }
+
+    ul, ol {
+        padding: 8px;
+        margin: 10px 0;
+        border: 2px solid lightgreen;
+    }
+    
+    ul li {
+        padding: 10px 0;
+        color: rgb(69, 168, 41);
+    }
+
+    ol li {
+        padding: 10px 0;
+        color: rgb(3, 209, 109);
+        list-style: none;
+    }
+
+    .for_list {
+        font-style: italic;
+        color: rgb(162, 141, 7);
+    }
+
+    .for_list span {
+        color: red;
+        font-weight: 600;
+    }
+
+    .meme {
+        width: 450px;
+        border: 4px dashed green;
+    }
+
+    button {
+        margin: 8px 0 0;
+        padding: 12px 18px;
+        font-weight: 300;
+        background-color: #6b9d2e;
+        color: #fff;
+        border-radius: 12px;
+    }
+
+    button:hover {
+        background-color: #537a23;
+    }
+
+    form {
+        max-width: 600px;
+        display: flex;
+        flex-direction: column;
+        row-gap: 8px;
+        padding: 20px;
+        border: 2px solid gray;
+        border-radius: 8px;
+    }
+
+    form button, form select {
+        max-width: 100px;
+    }
+
+    form label {
+        font-weight: 600;
+    }
+
+    form input {
+        margin-left: 6px;
+        padding: 4px;
+        border: 1px solid rgb(199, 199, 199);
+    }
+
+    form > *:not(button)::before {
+        content: '🔥';
+    }
+
+    .comment {
+        display: flex;
+        align-items: center;
+    }
+
+    form textarea {
+        padding: 4px;
+        margin-left: 8px;
+        border: 1px solid gray;
+    }
+
+    footer {
+        margin-top: 10px;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: rgb(241, 241, 241);
+    }
+
+    @media (max-width: 450px){
+
+        header, main, footer {
+            padding-right: 30px;
+            padding-left: 30px;
+        }
+
+        li {
+            font-size: 20px;
+        }
+    }
+
+
+</style>
